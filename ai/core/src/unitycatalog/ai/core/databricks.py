1161cd237a85ca93eeab5d26236af52b23faf7b1
aravind-segu <aravind.segu@databricks.com>
2025-02-27 18:00:03 -0800

    More prints

--- ai/core/src/unitycatalog/ai/core/databricks.py
+++ ai/core/src/unitycatalog/ai/core/databricks.py
@@ -251,10 +251,12 @@ class DatabricksFunctionClient(BaseFunctionClient):
         return self.spark.getActiveSession() is not None
 
     def set_default_spark_session(self):
+        print("DEFAULT SPARK SESSION")
         if not self._is_spark_session_active():
             _validate_databricks_connect_available()
             from databricks.connect.session import DatabricksSession as SparkSession
-
+            print("CLIENT")
+            print(self.client)
             if self.profile:
                 builder = SparkSession.builder.profile(self.profile)
             elif self.client is not None:
